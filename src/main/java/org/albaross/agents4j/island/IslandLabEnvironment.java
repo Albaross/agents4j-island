bbb8cfaf03818617fee4cf3a8f319883c4811386
Barbi <Manuel.Barbi@adesso.de>
2017-10-11 23:28:46 +0200

    update version

--- src/main/java/org/albaross/agents4j/island/IslandLabEnvironment.java
+++ src/main/java/org/albaross/agents4j/island/IslandLabEnvironment.java
@@ -15,7 +15,8 @@ import java.util.List;
 import java.util.Random;
 
 import org.albaross.agents4j.core.Agent;
-import org.albaross.agents4j.core.BasicEnvironment;
+import org.albaross.agents4j.learning.RLEnvironment;
+import org.albaross.agents4j.learning.utils.StateEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,13 +25,21 @@ import org.slf4j.LoggerFactory;
  * @author Manuel Barbi
  *
  */
-public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, IslandAction> {
+public class IslandLabEnvironment extends RLEnvironment<IslandPerception, IslandAction> implements StateEncoder<IslandPerception> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(IslandLabEnvironment.class);
 
 	protected Random rnd = new Random();
 
-	public static final int SITE_COMPLETE = 8 * 8;
+	public static final int FULL_BATTERY = 32;
+
+	public static final int NUM_PARTS = 8;
+	public static final int PART = 8;
+
+	public static final int SITE_COMPLETE = NUM_PARTS * PART;
+
+	public static final int RESOLUTION = 4;
+
 	protected int site;
 	protected boolean secured;
 
@@ -63,7 +72,13 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 
 	@Override
 	public IslandPerception createPerception(int agentId) {
-		return new IslandPerception(site / 16, secured, battery / 8, location, weather, prediction);
+		return new IslandPerception( //
+				(site * RESOLUTION) / SITE_COMPLETE, //
+				secured, //
+				(battery * RESOLUTION) / FULL_BATTERY, //
+				location, //
+				weather, //
+				prediction);
 	}
 
 	@Override
@@ -72,7 +87,7 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 		battery = Math.max(battery - 1, 0);
 
 		// keep state of site
-		int keepSiteState = site % 8;
+		int keepSiteState = site % PART;
 
 		exec(action);
 		env(agentId, keepSiteState);
@@ -87,7 +102,7 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 		case AT_HQ:
 			switch (action) {
 			case CHARGE_BATTERY:
-				battery = Math.min(battery + 8, 32);
+				battery = Math.min(battery + 8, FULL_BATTERY);
 				break;
 			case MOVE_TO_SITE:
 				location = (slow ? ON_THE_WAY_1 : ON_THE_WAY_2);
@@ -160,21 +175,21 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 		boolean shelter = (location == AT_HQ || location == IN_CAVE);
 
 		// assembled one component
-		reward = ((site % 8) > keepSiteState) ? 0.0 : -1.0;
+		reward = ((site % PART) > keepSiteState) ? 0.0 : -1.0;
 
 		// charge battery with solar panel
 		if (!shelter && weather == SUN)
-			battery = Math.min(battery + 2, 32);
+			battery = Math.min(battery + 2, FULL_BATTERY);
 
 		if (weather == THUNDERSTORM) {
 			// generate lightning
-			int loc = rnd.nextInt(12);
 			IslandLocation[] values = IslandLocation.values();
+			int loc = rnd.nextInt(2 * values.length);
 			IslandLocation lightning = (loc < values.length) ? values[loc] : null;
 
 			// site was struck by lightning
 			if (!secured && this.site < SITE_COMPLETE && AT_SITE.equals(lightning)) {
-				int damage = rnd.nextInt(16);
+				int damage = rnd.nextInt(2 * PART);
 				this.site = Math.max(this.site - damage, 0);
 				LOG.info("site was damaged");
 				reward = -10.0;
@@ -184,7 +199,7 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 			if (!shelter && location.equals(lightning)) {
 				LOG.info("agent {} was damaged", agentId);
 				location = AT_HQ;
-				battery = 32;
+				battery = FULL_BATTERY;
 				reward = -100.0;
 			}
 		}
@@ -193,7 +208,7 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 		if (battery == 0) {
 			LOG.info("agent {} ran out of energy", agentId);
 			location = AT_HQ;
-			battery = 32;
+			battery = FULL_BATTERY;
 			reward = -100.0;
 		}
 
@@ -225,15 +240,22 @@ public class IslandLabEnvironment extends BasicEnvironment<IslandPerception, Isl
 		this.weather = CLOUDS;
 		this.prediction = generateWeather();
 
-		this.battery = 32;
+		this.battery = FULL_BATTERY;
 		this.location = AT_HQ;
 
 		this.reward = 0.0;
 	}
 
 	@Override
-	public boolean supportsRL() {
-		return true;
+	public double[] encode(IslandPerception state) {
+		return new double[] { //
+				(double) (state.site + 1) / RESOLUTION, //
+				(double) (state.secured ? 1 : 0), //
+				(double) (state.battery + 1) / RESOLUTION, //
+				(double) (state.location.ordinal() + 1) / IslandLocation.values().length, //
+				(double) (state.weather.ordinal() + 1) / IslandWeather.values().length, //
+				(double) (state.prediction.ordinal() + 1) / IslandWeather.values().length //
+		};
 	}
 
 }
